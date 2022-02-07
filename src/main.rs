7cc3f099f36b2108facf5d9ea58a18e146fbb726
Christian <chriamue@gmail.com>
2022-02-07 20:51:05 +0100

    added v4l example

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use nokhwa::Camera;
 use nokhwa::CameraFormat;
+use nokhwa::CameraIndex;
 use nokhwa::FrameFormat;
 
 fn main() {
     let mut camera = Camera::new(
-        0,                                                                // index
-        Some(CameraFormat::new_from(1920, 1080, FrameFormat::MJPEG, 30)), // format
+        &CameraIndex::Index(0),                                           // index
+        Some(CameraFormat::new_from(1920, 1920, FrameFormat::MJPEG, 30)), // format
     )
     .unwrap();
+    //println!("{:?}", camera.supported_camera_controls().unwrap());
+    // open stream
     let res = camera.open_stream().unwrap();
     println!("{:?}", res);
-    loop {
+    for _ in 0..10 {
         let frame = camera.frame().unwrap();
         println!("{}, {}", frame.width(), frame.height());
         frame.save("frame.jpg").unwrap();
     }
+    let frame = camera.frame().unwrap();
+    println!("{}, {}", frame.width(), frame.height());
+    frame.save("frame.jpg").unwrap();
 }
